7ea56b87af0e579007bc33327535a7d51b7c0113
sidnei <sidneigoes23@gmail.com>
2025-03-15 15:12:56 -0300

    feat(api): adding Jpa repository, dependency injection and database seeding

---
+++ src/main/java/com/sidneigoes/project/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.sidneigoes.project.repositories;
+
+import com.sidneigoes.project.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
