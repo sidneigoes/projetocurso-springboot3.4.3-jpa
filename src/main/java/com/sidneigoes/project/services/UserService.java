c3810791dada6648358df51f5026fa4214ad9c9e
sidnei <sidneigoes23@gmail.com>
2025-03-15 15:33:42 -0300

    feat(api): implementing service layer and component registration. Created findAll() and findById() in UserService

---
+++ src/main/java/com/sidneigoes/project/services/UserService.java
@@ -0,0 +1,26 @@
+package com.sidneigoes.project.services;
+
+import com.sidneigoes.project.entities.User;
+import com.sidneigoes.project.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
