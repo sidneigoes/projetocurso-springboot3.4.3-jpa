da21b4af2e0b5026f9a6182550244e649e6890c8
sidnei <sidneigoes23@gmail.com>
2025-03-18 17:36:41 -0300

    feat(UserService): implementing new exception handler
    implementing new exception handler in the findById method
    
    Signed-off-by: sidnei <sidneigoes23@gmail.com>

--- src/main/java/com/sidneigoes/project/services/UserService.java
+++ src/main/java/com/sidneigoes/project/services/UserService.java
@@ -2,6 +2,7 @@ package com.sidneigoes.project.services;
 
 import com.sidneigoes.project.entities.User;
 import com.sidneigoes.project.repositories.UserRepository;
+import com.sidneigoes.project.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,8 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(()-> new ResourceNotFoundException(id));
+
     }
 
     public User insert(User obj){
