23a98d86f680fd1ee21bdd6d8bfbcabd322e5925
sidnei <sidneigoes23@gmail.com>
2025-03-18 10:29:12 -0300

    feat(api): implementing OrderItem class
    creating the associative entity with the OrderItam using OrderItemPK
    
    Signed-off-by: sidnei <sidneigoes23@gmail.com>

---
+++ src/main/java/com/sidneigoes/project/entities/OrderItem.java
@@ -0,0 +1,74 @@
+package com.sidneigoes.project.entities;
+
+import com.sidneigoes.project.entities.pk.OrderItemPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private OrderItemPK id;
+
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Order order, Product product,Double price, Integer quantity) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Order getOrder(){
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order){}
+
+    public Product getProduct(){
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product){}
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
